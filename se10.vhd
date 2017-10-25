5f8f41d43fb66a9476e1747a41eeb494a0fae396
chinmay <chinmay0301@gmail.com>
2017-10-25 22:11:37 +0530

    se 10 done

--- se10.vhd
+++ se10.vhd
@@ -14,10 +14,16 @@ architecture behave_se10 of se10 is
 
 begin 
  se_out(5 downto 0) <= se_in(5 downto 0); 
- se_out(15 downto 6) <="0000000000"; 
-
-
-
+ se_out(15) <= se_in(5); 
+ se_out(14) <= se_in(5); 
+ se_out(13) <= se_in(5); 
+ se_out(12) <= se_in(5); 
+ se_out(11) <= se_in(5); 
+ se_out(10) <= se_in(5); 
+ se_out(9) <= se_in(5); 
+ se_out(8) <= se_in(5);
+ se_out(7) <= se_in(5); 
+ se_out(6) <=se_in(5);
  end behave_se10; 
 
   
